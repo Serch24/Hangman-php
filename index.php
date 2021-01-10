8b9c057c6e4e21c79b6591d8e820506cd5709f1d
serch <duh>
2021-01-10 22:09:48 +0100

    estilos hechos

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Ahorcado</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <?php
@@ -13,7 +14,12 @@ $win = false;
 if (!isset($_COOKIE["inicio"])) {
     header("Location:Palabras.php");
 } else {
-    echo "<h1>Bienvenido, Adivina la palabra</h1>";
+    echo "<h1 class='bienvenida'>Adivina la palabra</h1>";
+    if(isset($_COOKIE['error'])){
+        echo "<h1 class='cant-vida'>Vidas: ". $_COOKIE['error']. "</h1>";
+    }else{
+        echo "<h1 class='cant-vida'>Vidas: 5</h1>";
+    }
 }
 
 /*Determina si las dos cadenas son iguales y ponde WIN en true*/
@@ -27,9 +33,11 @@ if (isset($_COOKIE["guiones"], $_COOKIE["palabra"])) {
 
 /*Muestra los guiones y caracteres si se ha adivinado la letra*/
 if (isset($_COOKIE["guiones"])) {
+    echo "<div class='guiones'>";
     for ($i = 0; $i < strlen($_COOKIE["guiones"]); $i++) {
-        echo substr($_COOKIE["guiones"], $i, 1) . "   ";
+        echo "<p>".substr($_COOKIE["guiones"], $i, 1)."</p>";
     }
+    echo "</div>";
 }
 
 /*Si se equivoca en escribir una palabra muestra un mensaje de error y elimina
@@ -37,28 +45,28 @@ if (isset($_COOKIE["guiones"])) {
  * */
 if (isset($_COOKIE["error_actived"])) {
     if ($_COOKIE["error"] == 0) {
-        echo "<h1>Pierdes todas las vidas!!!</h1>"; ?>
+        echo "<h1 class='todo-cambia'>Pierdes todas las vidas!!!</h1>"; ?>
                      <form method="POST" action="Palabras.php">
-                        <input type="submit" name="reiniciar" value="reiniciar" autofocus>
+                        <input class='form-reiniciar' type="submit" name="reiniciar" value="reiniciar" autofocus>
                      </form>
                   <?php
     } else {
         $vidas = $_COOKIE["error"];
-        echo "<h1>Te has equivocado, te quedan $vidas vidas.</h1>";
+        echo "<h1 class='error'>Te has equivocado, te quedan $vidas vidas.</h1>";
         setcookie("error_actived", "", time() - 1);
     }
 }
 
 if ($win) {
-    echo "<h1>Ganaste!!!!!!!</h1>"; ?>
+    echo "<div class='todo-cambia'>Ganaste!!!!!!!</div>"; ?>
                      <form method="POST" action="Palabras.php">
-                        <input type="submit" name="reiniciar" value="reiniciar" autofocus>
+                        <input class='form-reiniciar' type="submit" name="reiniciar" value="reiniciar" autofocus>
                      </form>
                   <?php
 } else {
      ?>
-  <form method="POST" action="Palabras.php">
-      <br><input type="text" name="letra" placeholder="escribe algo"  maxlength="1" required autofocus>
+  <form  class="form-inicio" method="POST" action="Palabras.php">
+       <input type="text" name="letra" placeholder="escribe algo"  maxlength="1" required autofocus>
      <input type="submit" name="enviar" value="enviar"> 
   </form>
 
